96c1d0b2c42e581d14344a3e78e0b2ea99cf7bbe
samuelshola777 <samuelshola14@gmail.com>
2023-07-09 22:24:19 +0100

    adding my models

---
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/service/AppUserService.java
@@ -0,0 +1,10 @@
+package com.example.megaPassworkSaver.service;
+
+import org.springframework.stereotype.Service;
+
+
+public interface AppUserService {
+
+
+
+}
---
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/serviceImpl/AppUserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.megaPassworkSaver.serviceImpl;
+
+import com.example.megaPassworkSaver.service.AppUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserServiceImpl implements AppUserService {
+}
