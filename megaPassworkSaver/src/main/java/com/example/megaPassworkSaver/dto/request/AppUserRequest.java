59efffdbd24408f52f9df1a36681d2abdaadef5c
samuelshola777 <samuelshola14@gmail.com>
2023-07-09 22:29:41 +0100

    adding my models

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/dto/request/AppUserRequest.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/dto/request/AppUserRequest.java
@@ -3,12 +3,11 @@ package com.example.megaPassworkSaver.dto.request;
 import com.example.megaPassworkSaver.data.model.Password;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.util.Set;
-
+@Data
 public class AppUserRequest {
-
-    private Set<Password> listOfPasswords;
     private String userName;
     private String emailAddress;
 }
--- megaPassworkSaver/src/test/java/com/example/megaPassworkSaver/service/AppUserServiceTest.java
+++ megaPassworkSaver/src/test/java/com/example/megaPassworkSaver/service/AppUserServiceTest.java
@@ -1,12 +1,25 @@
 package com.example.megaPassworkSaver.service;
 
+import com.example.megaPassworkSaver.dto.request.AppUserRequest;
+import jakarta.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@RequiredArgsConstructor
 class AppUserServiceTest {
+    @Nonnull
+    private final AppUserService appUserService;
+    private AppUserRequest appUserRequest1;
+    private AppUserRequest appUserRequest2;
+    private AppUserRequest appUserRequest3;
 
     @BeforeEach
     void setUp() {
+
+
     }
 }
