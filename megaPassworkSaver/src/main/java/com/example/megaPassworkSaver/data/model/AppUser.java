d00bda7a24ae081f102a4c55b91d6674a67f2a03
samuelshola777 <samuelshola14@gmail.com>
2023-07-09 21:14:32 +0100

    adding my models

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/AppUser.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/AppUser.java
@@ -1,6 +1,17 @@
 package com.example.megaPassworkSaver.data.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+@Entity
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private final List<Password> listOfPasswords;
     private String userName;
     private String unlockPassword;
 }
