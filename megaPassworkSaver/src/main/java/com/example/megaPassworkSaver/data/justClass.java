957200926c07cd1e2e8b0b05050ff63aae4cc607
samuelshola777 <samuelshola14@gmail.com>
2023-07-09 21:31:01 +0100

    adding my models

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/justClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.megaPassworkSaver.data;
-
-public class justClass {
-}
--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/AppUser.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/AppUser.java
@@ -2,18 +2,25 @@ package com.example.megaPassworkSaver.data.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.Set;
 
 @Entity
 @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @OneToMany( cascade = CascadeType.ALL, mappedBy = "churchBranch", orphanRemoval = true)
-    private final Set<Password> listOfPasswords;
+    @OneToMany( cascade = CascadeType.ALL, mappedBy = "listOfPasswords", orphanRemoval = true)
+    private  Set<Password> listOfPasswords;
     private String userName;
     private String unlockPassword;
+
+
 }
