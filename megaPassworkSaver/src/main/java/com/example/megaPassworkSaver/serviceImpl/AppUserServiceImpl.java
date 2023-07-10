70c35926f13b2d8675f2c846cc8bf2eb01cb5b83
samuelshola777 <samuelshola14@gmail.com>
2023-07-10 14:57:30 +0100

    testing and creating the register method

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/serviceImpl/AppUserServiceImpl.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/serviceImpl/AppUserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.megaPassworkSaver.serviceImpl;
 import com.example.megaPassworkSaver.data.repository.AppUserRepository;
 import com.example.megaPassworkSaver.dto.request.AppUserRequest;
 import com.example.megaPassworkSaver.dto.response.AppUserResponse;
+import com.example.megaPassworkSaver.exception.EmailAlreadyExistException;
 import com.example.megaPassworkSaver.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,8 @@ public class AppUserServiceImpl implements AppUserService {
 
         return null;
     }
-    private boolean ifEmailAlreadyExist(String emailAddress) {
-       if (!appUserRepository.findByEmailAddress(emailAddress)) return true;
-       return false;
+    private void ifEmailAlreadyExist(String emailAddress) {
+       if (!appUserRepository.findByEmailAddress(emailAddress)) throw new EmailAlreadyExistException("email already Exist");
+
     }
 }
