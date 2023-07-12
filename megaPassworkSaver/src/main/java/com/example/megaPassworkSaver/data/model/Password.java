163142aa4bbeab4a6c923bbd2192dc19f16eb3c0
samuelshola777 <samuelshola14@gmail.com>
2023-07-13 00:15:06 +0100

    just to push

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/Password.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/Password.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Password {
-    Facebook
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
