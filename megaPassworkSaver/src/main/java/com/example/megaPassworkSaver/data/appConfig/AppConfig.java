cda8f82710828a6020ec9073882e2df3bad09fb9
samuelshola777 <samuelshola14@gmail.com>
2023-07-15 04:43:48 +0100

    working on the access token

---
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/appConfig/AppConfig.java
@@ -0,0 +1,17 @@
+package com.example.megaPassworkSaver.data.appConfig;
+
+import com.example.megaPassworkSaver.data.repository.TokenRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Transactional
+public class AppConfig {
+
+//    @Bean
+//public TokenRepository tokenRepository(){
+//        return  TokenRepository();
+//    }
+
+}
