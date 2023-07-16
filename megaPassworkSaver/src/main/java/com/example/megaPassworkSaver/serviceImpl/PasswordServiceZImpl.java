a1a00664b5e3956afdb1f851710bb2635bf06d65
samuelshola777 <samuelshola14@gmail.com>
2023-07-16 18:58:44 +0100

    working on the access token

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/serviceImpl/PasswordServiceZImpl.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/serviceImpl/PasswordServiceZImpl.java
@@ -40,7 +40,9 @@ public class PasswordServiceZImpl implements PasswordServiceZ {
 
 
     private String encryptPassword(String password) {
-        byte[] encodedBytes = Base64.getEncoder().encode(password.getBytes(StandardCharsets.UTF_8));
+        String name = password.replace(" ","_______");
+
+        byte[] encodedBytes = Base64.getEncoder().encode(name.getBytes(StandardCharsets.UTF_8));
        return new String(encodedBytes);
     }
 public Password findPassword(String passwordLabel) {
