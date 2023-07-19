bdffffda4f6c1b651f40a1694f2498c455faa89e
samuelshola777 <samuelshola14@gmail.com>
2023-07-19 14:27:17 +0100

    starting the controller

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/AppUserController.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/AppUserController.java
@@ -1,9 +1,11 @@
 package com.example.megaPassworkSaver.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("api/passwordStorage")
+@RequiredArgsConstructor
 @RestController
 public class AppUserController {
 }
