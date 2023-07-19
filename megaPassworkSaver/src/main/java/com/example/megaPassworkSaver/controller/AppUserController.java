6fde21eaf45eb410046c8e04831a383856e7bb59
samuelshola777 <samuelshola14@gmail.com>
2023-07-19 14:34:24 +0100

    starting the controller

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/AppUserController.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/AppUserController.java
@@ -1,6 +1,11 @@
 package com.example.megaPassworkSaver.controller;
 
+import com.example.megaPassworkSaver.dto.request.AppUserRequest;
+import com.example.megaPassworkSaver.dto.response.AppUserResponse;
+import com.example.megaPassworkSaver.service.AppUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class AppUserController {
+    private final AppUserService appUserService;
+
+
+
+    public ResponseEntity<AppUserResponse> registerAppUser(@RequestBody AppUserRequest appUserRequest){
+
+    }
+
 }
