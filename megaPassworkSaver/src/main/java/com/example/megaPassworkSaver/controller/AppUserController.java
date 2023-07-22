465044232cb5d10feff922544f17b615f5c88bd9
samuelshola777 <samuelshola14@gmail.com>
2023-07-22 22:52:27 +0100

    working on registering controller

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/AppUserController.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/AppUserController.java
@@ -44,7 +44,7 @@ return new ResponseEntity<>(appUserService.generateAccessToken(passwordLabel, ap
 }
 @GetMapping("/userPassword")
 public ResponseEntity<UnlockPassword> unlockPassword(@RequestParam String passwordLabel, @RequestParam String token){
-return new ResponseEntity<>(appUserService.getPasswordByLabel(passwordLabel, token))
+return new ResponseEntity<>(appUserService.getPasswordByLabel(passwordLabel, token),HttpStatus.FOUND);
 }
 
 
