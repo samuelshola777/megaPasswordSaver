3163869b3376a2aa7a3a568b0ae1f3178d21eb4c
samuelshola777 <samuelshola14@gmail.com>
2023-07-25 14:50:00 +0100

    fixing the flyway query

--- megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
+++ megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS
+
 CREATE TABLE app_user(
     id SERIAL  PRIMARY KEY,
     email_address VARCHAR(255) NOT NULL,
     number_of_password INT,
-    role ENUM(),
+    role ENUM(30),
     unlock_password VARCHAR(255),
-    token VARCHAR(255),
+    token VARCHAR(255)
     );
 
-CREATE TABLE IF NOT EXISTS
+
 CREATE TABLE password(
     id SERIAL  PRIMARY KEY,
     last_updated_at DATETIME(19),
@@ -20,12 +20,11 @@ CREATE TABLE password(
     app_user_email VARCHAR(255) NOT NULL,
     token VARCHAR(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS
 CREATE TABLE token(
     id SERIAL  PRIMARY KEY,
-    expired_at TIMEDATE(19),
-    generated_at TIMEDATE(19),
-    token VARCHAR(255) NOT NULL
+    expired_at TIMESTAMP,
+    generated_at TIMESTAMP,
+    token VARCHAR(255) NOT NULL,
     password_id INT NOT NULL,
     FOREIGN KEY (password_id) REFERENCES password(password_id)
 );
