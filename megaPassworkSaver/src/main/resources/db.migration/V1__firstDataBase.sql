210f129c2e01b76a287bb62a4931bef66c19801b
samuelshola777 <samuelshola14@gmail.com>
2023-07-25 11:06:13 +0100

    working with flyway

--- megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
+++ megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
@@ -3,6 +3,7 @@ CREATE TABLE AppUser(
     id SERIAL  PRIMARY KEY,
     email_address VARCHAR(255) NOT NULL,
     number_of_password INT,
-
-
-);
+    role ENUM(),
+    unlock_password VARCHAR(255),
+    token VARCHAR(255),
+    );
