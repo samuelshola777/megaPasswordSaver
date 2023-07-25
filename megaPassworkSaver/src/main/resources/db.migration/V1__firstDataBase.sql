912cb2e75a8f504258529d5aa40d461edbc1a21b
samuelshola777 <samuelshola14@gmail.com>
2023-07-25 15:37:20 +0100

    fixing the flyway query

--- megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
+++ megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
@@ -3,7 +3,7 @@ CREATE TABLE app_user(
     id SERIAL  PRIMARY KEY,
     email_address VARCHAR(255) NOT NULL,
     number_of_password INT,
-    role ENUM(30),
+    role VARCHAR(255),
     unlock_password VARCHAR(255),
     token VARCHAR(255)
     );
