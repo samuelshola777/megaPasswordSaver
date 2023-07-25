a53e5e8f5882e42b6549a962dc9f69570b2dd7a7
samuelshola777 <samuelshola14@gmail.com>
2023-07-25 11:50:47 +0100

    working with flyway

--- megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
+++ megaPassworkSaver/src/main/resources/db.migration/V1__firstDataBase.sql
@@ -13,10 +13,17 @@ CREATE TABLE password(
     id SERIAL  PRIMARY KEY,
     last_updated_at DATETIME(19),
     created_at DATETIME(19),
-    password VARCHAR(255) NOT NULL ,
+    password TEXT NOT NULL ,
     password_label VARCHAR(255) NOT NULL,
     app_user_id INT NOT NULL,
     FOREIGN KEY (app_user_id) REFERENCES app_user(id),
     app_user_email VARCHAR(255) NOT NULL,
+    token VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS
+CREATE TABLE token(
+    id SERIAL  PRIMARY KEY,
+    expired_at TIMEDATE(19),
+    generated_at TIMEDATE(19),
     token VARCHAR(255) NOT NULL
 );
