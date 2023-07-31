66a736609bae9dffba407947481cef212cf15f61
samuelshola777 <samuelshola14@gmail.com>
2023-07-31 19:40:37 +0100

    there was an upgrade in the swagger ui so i'v gotten the new depndency

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/MegaPassworkSaverApplication.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/MegaPassworkSaverApplication.java
@@ -6,8 +6,7 @@ package com.example.megaPassworkSaver;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class MegaPassworkSaverApplication {
