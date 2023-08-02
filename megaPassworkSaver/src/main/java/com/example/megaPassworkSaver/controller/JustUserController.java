487dffa610e72b28f440f4424a1d6fc777682451
samuelshola777 <samuelshola14@gmail.com>
2023-08-02 15:31:54 +0100

    after completing the registration controller

--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/JustUserController.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/controller/JustUserController.java
@@ -2,12 +2,18 @@ package com.example.megaPassworkSaver.controller;
 
 import com.example.megaPassworkSaver.data.model.AppUser;
 import com.example.megaPassworkSaver.service.JustUserService;
+import com.example.megaPassworkSaver.serviceImpl.HttpResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +22,18 @@ public class JustUserController {
     private final JustUserService justUserService;
 
     @PostMapping("/createJustUser")
-    public ResponseEntity<?> registerJustUser(@RequestBody AppUser appUser){
-
+    public ResponseEntity<HttpResponse> registerJustUser(@RequestBody AppUser appUser){
+       AppUser  appUser1 = justUserService.registerUser(appUser);
+       return  ResponseEntity.created(URI.create("")).body(
+               HttpResponse.builder()
+                       .timeStamp(LocalDateTime.now().toString())
+                       .data(Map.of("user", appUser1))
+                       .message("app user created successfully")
+                       .status(HttpStatus.CREATED)
+                       .statusCode(HttpStatus.CREATED.value())
+                       .build());
     }
+
 
 
 }
--- megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/AppUser.java
+++ megaPassworkSaver/src/main/java/com/example/megaPassworkSaver/data/model/AppUser.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -15,6 +16,7 @@ import java.util.*;
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@SuperBuilder
 @Builder
 public class AppUser implements UserDetails {
 
